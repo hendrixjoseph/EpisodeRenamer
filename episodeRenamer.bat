05a89dd179d9e9629b8d2f8e889d15efad0981f4
hendrixjoseph <hendrixjoseph@aol.com>
2015-07-03 02:01:04 -0400

    Update episodeRenamer.bat

--- episodeRenamer.bat
+++ episodeRenamer.bat
@@ -3,11 +3,10 @@
 setlocal enabledelayedexpansion
 
 set season=3
-set filetype=wmv
+set filetype=avi
 
+rem If check is true, then the rename commands will be outputted rather than executed.
 set check=true
-
-set delim=+
 
 set old=0
 set new=0
@@ -22,9 +21,8 @@ for /R %%G in (*.%filetype%) do (
 )
 
 for /f "delims=" %%A in (episodeNames.txt) do (
-	set /a episodeNumber=!new!+1
-
-	set "newName=%season%.!episodeNumber! %%A.%filetype%"
+	
+	set "newName=%%A.%filetype%"
 	
 	set newNames[!new!]=!newName!
 	
@@ -36,6 +34,17 @@ if !old!==!new! (
 	set /a end=!old!-1
 	
 	for /L %%i in (0,1,!end!) do (
+	
+		set /a episodeNumber=%%i+1
+	
+		if !episodeNumber! GEQ 10 (
+			set zero=
+		) else (
+			set zero=0
+		)
+		
+		set "newNames[%%i]=%season%.!zero!!episodeNumber! !newNames[%%i]!"
+	
 		if "%check%"=="true" (
 			echo ren "!oldNames[%%i]!" "!newNames[%%i]!"
 		) else (
